627fc8a318a70827165b97f28ff54296a1076cde
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-04-29 12:57:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Cloud Application to circulate paper documents in soft copy form.
+
+[Initial Seed project](https://github.com/nikhilnxvverma1/angular2-express-orientjs-seed)
---
+++ server/typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/orientjs/index.d.ts" />
+/// <reference path="modules/bluebird/index.d.ts" />
+/// <reference path="modules/body-parser/index.d.ts" />
+/// <reference path="modules/dotenv/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/winston/index.d.ts" />
